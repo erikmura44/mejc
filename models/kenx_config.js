4281445f25ec5be508f9a6cb5220e1052c152ca4
erikmura44 <erikmura44@gmail.com>
2016-09-24 15:24:20 -0600

    .

--- models/kenx_config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const knex = requre('knex')
-const config = requre('../knexfile.js')
-const env = process.env.NODE_ENV || 'development'
-
-let pg = knex(config[env])
-
-module.exports = pg
