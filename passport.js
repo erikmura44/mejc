22b10d9e9462e1e2ed381e44668d085c0915c11a
erikmura44 <erikmura44@gmail.com>
2016-09-24 16:09:25 -0600

    added passport for auth

---
+++ passport.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const bcrypt = require("bcrypt");
+const passport = require("passport")
+const Local = require("passport-local")
+const indexModel = require('./models/index_query')
+
+
+passport.use(new Local((username, password, done) => {
+  indexModel.findUser(username)
+  .then((userData) => {
+    if(userData){
+      bcrypt.compare(password, userData.password,
+      function(err, res){
+        if(res) {
+          done(null, userData)
+        } else {
+          done(null, false)
+        }
+      })
+    }
+    else {
+      done(null, false)
+    }
+  })
+  .catch(function(err){
+    done(err)
+  })
+}))
+passport.serializeUser((userData, done) => {
+  done(null, userData.user_name)
+})
+// passport.deserializeUser((userData, done) => {
+//   done(null, userData);
+// }) // this deserializeUser() only captures user_name within req.body
+passport.deserializeUser((username, done) => {
+  userModel.findUser(username)
+  .then((userData) => {
+    done(null, userData)
+  })
+  .catch(function(err){
+    done(err)
+  })
+})
+module.exports = passport
