f8cfd772ecc8c687a502a829906328285fa81755
erikmura44 <erikmura44@gmail.com>
2016-09-26 13:59:00 -0600

    updated index and volunteers

--- controllers/volunteers.js
+++ controllers/volunteers.js
@@ -1,11 +1,15 @@
 'use strict'
 
 const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
+const indexModel = require('../models/index_query');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/volunteer', function(req, res, next) {
+  res.render('volunteer');
 });
 
 module.exports = router;
