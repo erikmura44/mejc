2a94d130c784f7d18df50b6af676214ca981a493
Ji Kim <jkim55@gmail.com>
2016-09-28 01:29:52 -0600

    added functions back into queries

--- model/test_queries.js
+++ model/test_queries.js
@@ -48,9 +48,64 @@ function filterEventbyCause_City(selectedCause, selectedCity){
     .where('event.city', selectedCity)
 }
 
+function filterOrganizationbyCity(city){
+  return knex('organization').where("city", city)
+    .select(
+      'organization.id',
+      'organization.organization_name',
+      'organization.city',
+      'organization.state'
+    )
+}
+
+function filterVolunteerbyCity(city){
+  return knex('volunteer').where("city", city)
+    .select(
+      'volunteer.id',
+      'volunteer.user_name',
+      'volunteer.city',
+      'volunteer.state'
+    )
+    .first()
+}
+
+function filterVolunteerbyCause(selectedCause){
+  return knex('cause')
+    .join('cause_volunteer','cause.id', 'cause_volunteer.cause_id')
+    .join('volunteer', 'cause_volunteer.volunteer_id', 'volunteer.id')
+    .select (
+      'volunteer.id',
+      'volunteer.user_name',
+      'volunteer.city',
+      'volunteer.state',
+      'cause.name'
+    )
+    .where('cause.name', selectedCause)
+}
+
+function filterVolunteerbyCause_City(selectedCause, selectedCity){
+  return knex('cause')
+    .join('cause_volunteer','cause.id', 'cause_volunteer.cause_id')
+    .join('volunteer', 'cause_volunteer.volunteer_id', 'volunteer.id')
+    .select (
+      'volunteer.id',
+      'volunteer.user_name',
+      'volunteer.city',
+      'volunteer.state',
+      'cause.name'
+    )
+    .where('cause.name', selectedCause)
+    .where('volunteer.city', selectedCity)
+}
+
 module.exports = {
   filterEventbyCity: filterEventbyCity,
   filterEventbyCause: filterEventbyCause,
-  filterEventbyCause_City: filterEventbyCause_City
+  filterEventbyCause_City: filterEventbyCause_City,
 
+  filterOrganizationbyCity: filterOrganizationbyCity,
+
+  filterVolunteerbyCity: filterVolunteerbyCity,
+  filterVolunteerbyCause: filterVolunteerbyCause,
+  filterVolunteerbyCause_City: filterVolunteerbyCause_City
 }
